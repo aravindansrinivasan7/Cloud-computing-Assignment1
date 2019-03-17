ca21acd5416b6e0ed71fcef2784cc6f6208871a5
aravindan <aravindan81222@gmail.com>
2019-03-17 10:01:25 +0000

    Update edit.py

--- MYGPU/edit.py
+++ MYGPU/edit.py
@@ -17,7 +17,8 @@ class Edit(webapp2.RequestHandler):
         user=users.get_current_user()
         myuser_key=ndb.Key('MyGPU',user.user_id())
         myuser=myuser_key.get()
-        template_values={'myuser':myuser}
+        a='hello'
+        template_values={'myuser':myuser,'a':a}
         template=JINJA_ENVIRONMENT.get_template('edit.html')
         self.response.write(template.render(template_values))
     def post(self):
